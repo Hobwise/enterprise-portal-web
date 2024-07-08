60f4f1d984b24a76b707eb01dcdc792a7161adae
Damilare Oyedeji <damoye81@gmail.com>
2024-07-08 18:51:23 +0100

    update

---
+++ app/auth/layout.tsx
@@ -0,0 +1,20 @@
+import { inter } from '@/utilities/ui-config/fonts';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+export const metadata = {
+  title: 'Hobink | Dashboard',
+  description: 'Streamline your business processes',
+};
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en' className={`${inter.className} `} suppressHydrationWarning>
+      <body>{children}</body>
+    </html>
+  );
+}
