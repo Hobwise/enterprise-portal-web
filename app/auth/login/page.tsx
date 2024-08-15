c95896b64a7ba1ab1aaafc5a909f9c436497c253
Damilare Oyedeji <damoye81@gmail.com>
2024-08-15 22:24:32 +0100

    update

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,9 +1,9 @@
-import { Spacer } from '@nextui-org/react';
-import Link from 'next/link';
 import HobinkLogo from '@/components/logo';
 import LoginForm from '@/components/ui/auth/loginForm';
-import Background from '../../../public/assets/images/nightlife-party-with-drinks-bar 1.png';
+import { Spacer } from '@nextui-org/react';
 import Image from 'next/image';
+import Link from 'next/link';
+import Background from '../../../public/assets/images/nightlife-party-with-drinks-bar 1.png';
 
 export const metadata = {
   title: 'Log in to Hobink',
@@ -13,19 +13,19 @@ export const metadata = {
 export default function Login() {
   return (
     <main
-      className={`background relative xl:py-8 py-2  flex justify-center xl:items-center items-start   min-h-screen `}
+      className={`background relative lg:py-8 py-2 flex justify-center lg:items-center items-start   min-h-screen `}
     >
       <Image fill className='absolute' src={Background} alt='background' />
       <div className='container flex flex-col justify-center  w-full xl:p-0 px-4 '>
         <HobinkLogo
           textColor='text-white'
-          containerClass='flex xl:hidden gap-2 z-10 items-center pt-8 mb-10'
+          containerClass='flex lg:hidden gap-2 z-10 items-center pt-8 mb-10'
         />
-        <div className='flex flex-col justify-between xl:flex-row'>
-          <div className='relative w-[400px] xl:flex flex-col  hidden  text-white'>
+        <div className='flex flex-col justify-between gap-4 lg:flex-row'>
+          <div className='relative w-[400px] lg:flex flex-col  hidden  text-white'>
             <HobinkLogo
               textColor='text-white'
-              containerClass='xl:flex hidden gap-2 '
+              containerClass='lg:flex hidden gap-2 '
             />
             <div className='absolute bottom-0'>
               <h2 className='text-[32px] font-[600]  leading-9 mb-3'>
@@ -38,11 +38,12 @@ export default function Login() {
             </div>
           </div>
 
-          <div className='flex-1 z-10  mb-10 xl:max-w-[456px] w-full md:mb-0 bg-white text-black lg:p-7 py-12 px-6  rounded-xl'>
+          <div className='flex-1 z-10  mb-10 lg:max-w-[456px] w-full md:mb-0 bg-white text-black lg:p-7 py-12 px-6  rounded-xl'>
             <h2 className='text-[28px] font-bold mb-2'>Log In</h2>
             <p className='text-sm  text-grey500 mb-14'>
               Enter your credentials to access your account
             </p>
+
             <LoginForm />
             <Spacer y={8} />
             <div className='flex items-center gap-2'>
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,9 +1,9 @@
-import { Spacer } from '@nextui-org/react';
-import Link from 'next/link';
 import HobinkLogo from '@/components/logo';
 import SignupForm from '@/components/ui/auth/signupForm';
-import Background from '../../../public/assets/images/nightlife-party-with-drinks-bar 1.png';
+import { Spacer } from '@nextui-org/react';
 import Image from 'next/image';
+import Link from 'next/link';
+import Background from '../../../public/assets/images/nightlife-party-with-drinks-bar 1.png';
 
 export const metadata = {
   title: 'Hobink | Create account',
@@ -20,20 +20,20 @@ export const metadata = {
 export default function Signup() {
   return (
     <main
-      className={` background relative xl:py-8 py-2   flex justify-center xl:items-center items-start  min-h-screen `}
+      className={` background relative lg:py-8 py-2   flex justify-center lg:items-center items-start  min-h-screen `}
     >
       <Image fill className='absolute' src={Background} alt='background' />
       <div className='container flex flex-col justify-center  w-full xl:p-0 px-4 '>
         <HobinkLogo
           textColor='text-white'
-          containerClass='flex xl:hidden z-10 gap-2 items-center pt-8 mb-10'
+          containerClass='flex lg:hidden z-10 gap-2 items-center pt-8 mb-10'
         />
-        <div className='flex w-full flex-col  justify-between xl:flex-row'>
+        <div className='flex w-full flex-col  justify-between lg:flex-row'>
           {/* Left Card: Text Content */}
-          <div className='relative w-[400px] xl:flex flex-col  hidden  text-white'>
+          <div className='relative w-[400px] lg:flex flex-col  hidden  text-white'>
             <HobinkLogo
               textColor='text-white'
-              containerClass='xl:flex hidden gap-2 '
+              containerClass='lg:flex hidden gap-2 '
             />
             <div className='absolute bottom-0'>
               <h2 className='text-[32px] font-[600]  leading-9 mb-3'>
@@ -47,7 +47,7 @@ export default function Signup() {
           </div>
 
           {/* Right Card: Login Form */}
-          <div className='flex-1 z-10  mb-10 xl:max-w-[456px]  w-full md:mb-0 bg-white text-black lg:p-7 py-12 px-6  rounded-xl'>
+          <div className='flex-1 z-10  mb-10 lg:max-w-[456px]  w-full md:mb-0 bg-white text-black lg:p-7 py-12 px-6  rounded-xl'>
             <h2 className='text-[28px] font-bold mb-2'>Create Account</h2>
             <p className='text-sm  text-grey500 mb-8'>
               Enter your details to create your account
