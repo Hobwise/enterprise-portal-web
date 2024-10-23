e5afa76d2c77d688568077494b99ad889882be15
Damilare Oyedeji <damoye81@gmail.com>
2024-10-23 18:05:18 +0100

    update

--- app/auth/forget-password/page.tsx
+++ app/auth/forget-password/page.tsx
@@ -1,9 +1,7 @@
 import BackButton from '@/components/backButton';
 import CompanyLogo from '@/components/logo';
 import EntryPoint from '@/components/ui/auth/forget-password/EntryPoint';
-import Image from 'next/image';
 import { companyInfo } from '../../../lib/companyInfo';
-import Background from '../../../public/assets/images/nightlife-party-with-drinks-bar 1.png';
 
 export const metadata = {
   title: `${companyInfo.name} | Create account`,
@@ -11,19 +9,12 @@ export const metadata = {
 };
 const ForgetPassword = () => {
   return (
-    <main className='min-h-screen background flex flex-col md:justify-center justify-start items-center md:p-0 py-24 px-4 '>
-      <Image
-        fill
-        className='absolute bg-no-repeat h-full   w-full bg-cover'
-        src={Background}
-        alt='background'
-      />
+    <main className='min-h-screen bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]  bg-pink200 flex flex-col md:justify-center justify-start items-center md:p-0 py-24 px-4 '>
+      <CompanyLogo />
       <div className='absolute top-0 left-0'>
-        <BackButton color='text-white' url='/auth/login' />
+        <BackButton color='text-black' url='/auth/login' />
       </div>
       <div className='md:w-[464px] w-full z-10 bg-white text-black lg:p-7 py-12 px-6  md:rounded-2xl rounded-lg'>
-        <CompanyLogo />
-
         <EntryPoint />
       </div>
     </main>
