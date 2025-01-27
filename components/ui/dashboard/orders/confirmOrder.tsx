1fed94f073f4e121badcb2ad0bc75b21ea87f7e5
Damilare Oyedeji <damoye81@gmail.com>
2025-01-27 10:02:54 +0100

    update

--- components/ui/dashboard/orders/confirmOrder.tsx
+++ components/ui/dashboard/orders/confirmOrder.tsx
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 import {
   completeOrder,
   getOrder,
-} from '@/app/api/controllers/dashboard/orders';
-import { CustomInput } from '@/components/CustomInput';
-import { CustomButton } from '@/components/customButton';
-import { formatPrice, getJsonItemFromLocalStorage, notify } from '@/lib/utils';
+} from "@/app/api/controllers/dashboard/orders";
+import { CustomInput } from "@/components/CustomInput";
+import { CustomButton } from "@/components/customButton";
+import { formatPrice, getJsonItemFromLocalStorage, notify } from "@/lib/utils";
 import {
   Divider,
   Modal,
@@ -14,13 +14,13 @@ import {
   ModalHeader,
   Spacer,
   Spinner,
-} from '@nextui-org/react';
-import Image from 'next/image';
-import { useEffect, useState } from 'react';
-import { HiArrowLongLeft } from 'react-icons/hi2';
-import { IoIosArrowRoundBack } from 'react-icons/io';
-import { MdKeyboardArrowRight } from 'react-icons/md';
-import noImage from '../../../../public/assets/images/no-image.svg';
+} from "@nextui-org/react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { HiArrowLongLeft } from "react-icons/hi2";
+import { IoIosArrowRoundBack } from "react-icons/io";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import noImage from "../../../../public/assets/images/no-image.svg";
 
 const ConfirmOrderModal = ({
   singleOrder,
@@ -28,12 +28,12 @@ const ConfirmOrderModal = ({
   toggleConfirmModal,
   refetch,
 }: any) => {
-  const userInformation = getJsonItemFromLocalStorage('userInformation');
+  const userInformation = getJsonItemFromLocalStorage("userInformation");
   const [isLoading, setIsLoading] = useState(false);
 
   const [screen, setScreen] = useState(1);
   const [order, setOrder] = useState([]);
-  const [reference, setReference] = useState('');
+  const [reference, setReference] = useState("");
 
   const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(0);
 
@@ -46,14 +46,14 @@ const ConfirmOrderModal = ({
   };
 
   const paymentMethods = [
-    { text: 'Pay with cash', subText: ' Accept payment using cash', id: 0 },
-    { text: 'Pay with Pos', subText: ' Accept payment using Pos', id: 1 },
+    { text: "Pay with cash", subText: " Accept payment using cash", id: 0 },
+    { text: "Pay with Pos", subText: " Accept payment using Pos", id: 1 },
     {
-      text: 'Pay with bank transfer',
-      subText: 'Accept payment via bank transfer',
+      text: "Pay with bank transfer",
+      subText: "Accept payment via bank transfer",
       id: 2,
     },
-    { text: 'Pay Later', subText: 'Keep this order open', id: 3 },
+    { text: "Pay Later", subText: "Keep this order open", id: 3 },
   ];
 
   const getOrderDetails = async () => {
@@ -80,17 +80,17 @@ const ConfirmOrderModal = ({
 
     if (data?.data?.isSuccessful) {
       notify({
-        title: 'Payment made!',
-        text: 'Payment has been made, awaiting confirmation',
-        type: 'success',
+        title: "Payment made!",
+        text: "Payment has been made, awaiting confirmation",
+        type: "success",
       });
       toggleConfirmModal();
       refetch();
     } else if (data?.data?.error) {
       notify({
-        title: 'Error!',
+        title: "Error!",
         text: data?.data?.error,
-        type: 'error',
+        type: "error",
       });
     }
   };
@@ -103,7 +103,7 @@ const ConfirmOrderModal = ({
   return (
     <Modal
       isDismissable={false}
-      size={screen === 1 ? '4xl' : 'md'}
+      size={screen === 1 ? "4xl" : "md"}
       isOpen={isOpenConfirmOrder}
       onOpenChange={() => {
         setScreen(1);
@@ -111,7 +111,7 @@ const ConfirmOrderModal = ({
         refetch();
         setSelectedPaymentMethod(0);
         toggleConfirmModal();
-        setReference('');
+        setReference("");
         setOrder([]);
       }}
     >
@@ -120,49 +120,49 @@ const ConfirmOrderModal = ({
           <>
             {screen === 1 && (
               <>
-                <ModalHeader className='flex flex-col mt-3 gap-1'>
-                  <div className='flex flex-row flex-wrap  justify-between'>
+                <ModalHeader className="flex flex-col mt-3 gap-1">
+                  <div className="flex flex-row flex-wrap  justify-between">
                     <div>
-                      <div className='text-[24px] leading-8 font-semibold'>
-                        <span className='text-black'>
+                      <div className="text-[24px] leading-8 font-semibold">
+                        <span className="text-black">
                           {singleOrder.reference}
                         </span>
                       </div>
-                      <p className='text-sm  text-grey600 xl:mb-8 w-full mb-4'>
+                      <p className="text-sm  text-grey600 xl:mb-8 w-full mb-4">
                         Confirm order before checkout
                       </p>
                     </div>
-                    <div className='flex  gap-3'>
+                    <div className="flex  gap-3">
                       <CustomButton
                         onClick={toggleConfirmModal}
-                        className='py-2 px-4 mb-0 bg-white border border-primaryGrey'
+                        className="py-2 px-4 mb-0 bg-white border border-primaryGrey"
                       >
                         Update order
                       </CustomButton>
 
                       <CustomButton
                         onClick={() => setScreen(2)}
-                        className='py-2 px-4 mb-0 text-white'
-                        backgroundColor='bg-primaryColor'
+                        className="py-2 px-4 mb-0 text-white"
+                        backgroundColor="bg-primaryColor"
                       >
-                        <div className='flex gap-2 items-center justify-center'>
+                        <div className="flex gap-2 items-center justify-center">
                           <p>
-                            Checkout {formatPrice(singleOrder.totalAmount)}{' '}
+                            Checkout {formatPrice(singleOrder.totalAmount)}{" "}
                           </p>
-                          <HiArrowLongLeft className='text-[22px] rotate-180' />
+                          <HiArrowLongLeft className="text-[22px] rotate-180" />
                         </div>
                       </CustomButton>
                     </div>
                   </div>
-                  <Divider className='bg-primaryGrey' />
+                  <Divider className="bg-primaryGrey" />
                 </ModalHeader>
                 <ModalBody>
-                  <div className='flex lg:flex-row flex-col gap-3 mb-4'>
-                    <div className='lg:w-[60%] max-h-[300px] overflow-y-scroll  w-full rounded-lg border border-[#E4E7EC80] p-2 '>
+                  <div className="flex lg:flex-row flex-col gap-3 mb-4">
+                    <div className="lg:w-[60%] max-h-[300px] overflow-y-scroll  w-full rounded-lg border border-[#E4E7EC80] p-2 ">
                       {order.length === 0 ? (
                         <div className={`grid h-full place-content-center`}>
                           <Spinner />
-                          <p className='text-center mt-1 text-[14px] text-grey400'>
+                          <p className="text-center mt-1 text-[14px] text-grey400">
                             Fetching order details...
                           </p>
                         </div>
@@ -173,9 +173,9 @@ const ConfirmOrderModal = ({
                               <>
                                 <div
                                   key={item.id}
-                                  className='flex justify-between'
+                                  className="flex justify-between"
                                 >
-                                  <div className='w-[250px] rounded-lg text-black  flex'>
+                                  <div className="w-[250px] rounded-lg text-black  flex">
                                     <div
                                       className={`grid place-content-center`}
                                     >
@@ -188,39 +188,42 @@ const ConfirmOrderModal = ({
                                         width={60}
                                         height={60}
                                         className={
-                                          'bg-cover h-[60px] rounded-lg w-[60px]'
+                                          "bg-cover h-[60px] rounded-lg w-[60px]"
                                         }
-                                        aria-label='uploaded image'
-                                        alt='uploaded image(s)'
+                                        aria-label="uploaded image"
+                                        alt="uploaded image(s)"
                                       />
                                     </div>
-                                    <div className='p-3 flex  flex-col text-sm justify-center'>
-                                      <p className='font-[600]'>
+                                    <div className="p-3 flex  flex-col text-sm justify-center">
+                                      <p className="font-[600]">
                                         {item.menuName}
                                       </p>
                                       <Spacer y={2} />
-                                      <p className='text-grey600'>
-                                        {item.itemName}
+                                      <p className="text-grey600">
+                                        {item.itemName}{" "}
+                                        <span className="text-black">
+                                          {item.unit && `(${item.unit})`}
+                                        </span>
                                       </p>
 
-                                      <p className=''>{item.iquantity}</p>
+                                      <p className="">{item.iquantity}</p>
                                     </div>
                                   </div>
-                                  <div className='text-black flex items-center text-[12px]'>
+                                  <div className="text-black flex items-center text-[12px]">
                                     <span>QTY:</span>
-                                    <span className='font-[600]'>
-                                      {' '}
+                                    <span className="font-[600]">
+                                      {" "}
                                       {item.quantity}
                                     </span>
                                   </div>
-                                  <div className='text-black w-[150px] grid place-content-center'>
-                                    <h3 className='font-[600]'>
+                                  <div className="text-black w-[150px] grid place-content-center">
+                                    <h3 className="font-[600]">
                                       {formatPrice(item?.unitPrice)}
                                     </h3>
                                   </div>
                                 </div>
                                 {index !== order?.orderDetails?.length - 1 && (
-                                  <Divider className='bg-primaryGrey' />
+                                  <Divider className="bg-primaryGrey" />
                                 )}
                               </>
                             );
@@ -228,29 +231,29 @@ const ConfirmOrderModal = ({
                         </>
                       )}
                     </div>
-                    <div className='flex-grow bg-[#F7F6FA] z-10 rounded-lg p-4'>
-                      <div className='flex justify-between items-center'>
-                        <div className='text-sm'>
-                          <p className='font-[600] text-black'>
+                    <div className="flex-grow bg-[#F7F6FA] z-10 rounded-lg p-4">
+                      <div className="flex justify-between items-center">
+                        <div className="text-sm">
+                          <p className="font-[600] text-black">
                             {singleOrder.placedByName}
                           </p>
-                          <p className='text-grey500'>
+                          <p className="text-grey500">
                             {singleOrder.placedByPhoneNumber}
                           </p>
                         </div>
                         <div>
-                          <span className='rounded-full text-sm px-4 py-2 bg-[#EAE5FF] text-primaryColor'>
+                          <span className="rounded-full text-sm px-4 py-2 bg-[#EAE5FF] text-primaryColor">
                             {singleOrder.qrReference}
                           </span>
                         </div>
                       </div>
                       <Spacer y={5} />
-                      <div className='text-sm'>
-                        <p className='font-[600] text-black'>Comment</p>
-                        <p className='text-grey500'>
+                      <div className="text-sm">
+                        <p className="font-[600] text-black">Comment</p>
+                        <p className="text-grey500">
                           {singleOrder.comment
                             ? singleOrder.comment
-                            : 'no comment'}
+                            : "no comment"}
                         </p>
                       </div>
                     </div>
@@ -259,29 +262,29 @@ const ConfirmOrderModal = ({
               </>
             )}
             {screen === 2 && (
-              <div className='p-5'>
+              <div className="p-5">
                 <div>
-                  <div className=' text-[18px] leading-8 font-semibold'>
-                    <span className='text-black'>Select payment method</span>
+                  <div className=" text-[18px] leading-8 font-semibold">
+                    <span className="text-black">Select payment method</span>
                   </div>
-                  <p className='text-sm  text-primaryColor xl:mb-8 w-full mb-4'>
+                  <p className="text-sm  text-primaryColor xl:mb-8 w-full mb-4">
                     {formatPrice(singleOrder.totalAmount)}
                   </p>
                 </div>
-                <div className='flex flex-col gap-1 text-black'>
+                <div className="flex flex-col gap-1 text-black">
                   {paymentMethods.map((item) => (
                     <div
                       key={item.id}
                       onClick={() => handleClick(item.id)}
                       className={`flex  cursor-pointer items-center gap-2 p-4 rounded-lg justify-between  ${
                         selectedPaymentMethod === item.id
-                          ? 'bg-[#EAE5FF80]'
-                          : ''
+                          ? "bg-[#EAE5FF80]"
+                          : ""
                       } `}
                     >
                       <div>
-                        <p className='font-semibold'>{item.text}</p>
-                        <p className='text-sm text-grey500'>{item.subText}</p>
+                        <p className="font-semibold">{item.text}</p>
+                        <p className="text-sm text-grey500">{item.subText}</p>
                       </div>
                       <MdKeyboardArrowRight />
                     </div>
@@ -295,15 +298,15 @@ const ConfirmOrderModal = ({
                   onClick={() => setScreen(2)}
                   className={`p-4 cursor-pointer text-black flex items-center`}
                 >
-                  <IoIosArrowRoundBack className='text-2xl' />
-                  <span className='text-sm '>Back</span>
+                  <IoIosArrowRoundBack className="text-2xl" />
+                  <span className="text-sm ">Back</span>
                 </div>
-                <div className='px-5 pb-5'>
+                <div className="px-5 pb-5">
                   <div>
-                    <div className=' text-[18px] leading-8 font-semibold'>
-                      <span className='text-black'>Confirm payment</span>
+                    <div className=" text-[18px] leading-8 font-semibold">
+                      <span className="text-black">Confirm payment</span>
                     </div>
-                    <p className='text-sm  text-grey500 xl:mb-8 w-full mb-4'>
+                    <p className="text-sm  text-grey500 xl:mb-8 w-full mb-4">
                       confirm that customer has paid for order
                     </p>
                   </div>
@@ -311,9 +314,9 @@ const ConfirmOrderModal = ({
                     className={`flex items-center gap-2 p-4 rounded-lg justify-between bg-[#EAE5FF80]`}
                   >
                     <div>
-                      <p className='text-sm text-grey500'>TOTAL ORDER</p>
-                      <p className='font-bold text-black text-[20px]'>
-                        {' '}
+                      <p className="text-sm text-grey500">TOTAL ORDER</p>
+                      <p className="font-bold text-black text-[20px]">
+                        {" "}
                         {formatPrice(singleOrder.totalAmount)}
                       </p>
                     </div>
@@ -321,19 +324,19 @@ const ConfirmOrderModal = ({
                   </div>
                   <Spacer y={4} />
                   <CustomInput
-                    type='text'
+                    type="text"
                     // defaultValue={menuItem?.itemName}
                     value={reference}
                     onChange={(e) => setReference(e.target.value)}
-                    name='itemName'
-                    label='Enter ref'
-                    placeholder='Provide payment reference'
+                    name="itemName"
+                    label="Enter ref"
+                    placeholder="Provide payment reference"
                   />
                   <Spacer y={5} />
-                  <div className='flex gap-5'>
+                  <div className="flex gap-5">
                     <CustomButton
                       onClick={toggleConfirmModal}
-                      className='bg-white h-[50px] w-full border border-primaryGrey'
+                      className="bg-white h-[50px] w-full border border-primaryGrey"
                     >
                       Cancel
                     </CustomButton>
@@ -341,15 +344,15 @@ const ConfirmOrderModal = ({
                       loading={isLoading}
                       disabled={isLoading}
                       onClick={finalizeOrder}
-                      className='text-white w-full h-[50px]'
+                      className="text-white w-full h-[50px]"
                     >
-                      <div className='flex gap-2 items-center justify-center'>
-                        <p>{'Confirm payment'} </p>
-                        <HiArrowLongLeft className='text-[22px] rotate-180' />
+                      <div className="flex gap-2 items-center justify-center">
+                        <p>{"Confirm payment"} </p>
+                        <HiArrowLongLeft className="text-[22px] rotate-180" />
                       </div>
                     </CustomButton>
                   </div>
-                </div>{' '}
+                </div>{" "}
               </>
             )}
           </>
