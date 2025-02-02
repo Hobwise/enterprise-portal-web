24798fe7180aec389d4aa1b68eb8da19213a32fa
Damilare Oyedeji <damoye81@gmail.com>
2025-02-02 13:13:26 +0100

    update

--- components/ui/dashboard/settings/BillingsComponents/BillingHistory.tsx
+++ components/ui/dashboard/settings/BillingsComponents/BillingHistory.tsx
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback } from "react";
 import {
   Table,
   TableBody,
@@ -15,14 +15,14 @@ import {
   Chip,
   Spinner,
   Pagination,
-} from '@nextui-org/react';
-import { AiOutlineCloudDownload } from 'react-icons/ai';
-import { VscEye } from 'react-icons/vsc';
-import InvoiceSection from './Invoice';
-import moment from 'moment';
-import { addCommasToNumber } from '@/lib/utils';
-import usePagination from '@/hooks/usePagination';
-import CustomPagination from './CustomPagination';
+} from "@nextui-org/react";
+import { AiOutlineCloudDownload } from "react-icons/ai";
+import { VscEye } from "react-icons/vsc";
+import InvoiceSection from "./Invoice";
+import moment from "moment";
+import { addCommasToNumber } from "@/lib/utils";
+import usePagination from "@/hooks/usePagination";
+import CustomPagination from "./CustomPagination";
 
 const SubscriptionTable = ({ subscriptions, searchQuery }: any) => {
   const [filteredData, setFilteredData] = useState(subscriptions);
@@ -41,7 +41,7 @@ const SubscriptionTable = ({ subscriptions, searchQuery }: any) => {
     if (subscriptions && searchQuery) {
       const filtered = subscriptions.filter((item: any) =>
         Object.values(item)
-          .join(' ')
+          .join(" ")
           .toLowerCase()
           .includes(searchQuery.toLowerCase())
       );
@@ -72,20 +72,20 @@ const SubscriptionTable = ({ subscriptions, searchQuery }: any) => {
   };
 
   const INITIAL_VISIBLE_COLUMNS = [
-    'plan',
-    'billDate',
-    'amount',
-    'duration',
-    'invoice',
+    "plan",
+    "billDate",
+    "amount",
+    "duration",
+    "invoice",
   ];
 
   const columns = [
-    { name: 'ID', uid: 'id' },
-    { name: 'Plan', uid: 'plan' },
-    { name: 'Bill Date', uid: 'billDate' },
-    { name: 'Amount', uid: 'amount' },
-    { name: 'Duration', uid: 'duration' },
-    { name: 'Invoice', uid: 'invoice' },
+    { name: "ID", uid: "id" },
+    { name: "Plan", uid: "plan" },
+    { name: "Bill Date", uid: "billDate" },
+    { name: "Amount", uid: "amount" },
+    { name: "Duration", uid: "duration" },
+    { name: "Invoice", uid: "invoice" },
     // { name: '', uid: 'actions' },
   ];
 
@@ -102,34 +102,34 @@ const SubscriptionTable = ({ subscriptions, searchQuery }: any) => {
   } = usePagination(subscriptions, columns, INITIAL_VISIBLE_COLUMNS);
 
   const mapPlan = (plan: number) => {
-    return ['Unknown', 'Basic', 'Professional', 'Premium'][plan] || 'Unknown';
+    return ["Unknown", "Basic", "Professional", "Premium"][plan] || "Unknown";
   };
 
   const mapAmount = (amount: number) => `₦${addCommasToNumber(amount)}`;
 
   const mapPaymentPeriod = (paymentPeriod: number) =>
-    paymentPeriod === 0 ? 'Monthly' : 'Annually';
+    paymentPeriod === 0 ? "Monthly" : "Annually";
 
   const renderCell = useCallback(
     (item: any, columnKey: string) => {
       switch (columnKey) {
-        case 'subscriptionEndDate':
+        case "subscriptionEndDate":
           return moment(item[columnKey]).isValid()
-            ? moment(item[columnKey]).format('DD/MM/YYYY hh:mm A')
-            : 'N/A';
-        case 'isActive':
+            ? moment(item[columnKey]).format("DD/MM/YYYY hh:mm A")
+            : "N/A";
+        case "isActive":
           return (
             <Chip
               className={`text-xs h-6 font-bold ${
                 item[columnKey]
-                  ? 'bg-green-100 text-green-700'
-                  : 'bg-gray-100 text-gray-700'
+                  ? "bg-green-100 text-green-700"
+                  : "bg-gray-100 text-gray-700"
               }`}
             >
-              {item[columnKey] ? 'Active' : 'Inactive'}
+              {item[columnKey] ? "Active" : "Inactive"}
             </Chip>
           );
-        case 'invoice':
+        case "invoice":
           return (
             <div className="flex flex-row gap-3">
               <button
@@ -153,14 +153,14 @@ const SubscriptionTable = ({ subscriptions, searchQuery }: any) => {
               </button>
             </div>
           );
-        case 'plan':
+        case "plan":
           return mapPlan(item[columnKey]);
-        case 'amount':
+        case "amount":
           return mapAmount(item[columnKey]);
-        case 'paymentPeriod':
+        case "paymentPeriod":
           return mapPaymentPeriod(item[columnKey]);
         default:
-          return item[columnKey] || 'N/A';
+          return item[columnKey] || "N/A";
       }
     },
     [downloadingInvoiceId]
@@ -173,7 +173,7 @@ const SubscriptionTable = ({ subscriptions, searchQuery }: any) => {
 
     // Simulate an async operation (e.g., API call to download the invoice)
     setTimeout(() => {
-      console.log('Invoice downloaded:', details?.id);
+      console.log("Invoice downloaded:", details?.id);
       setDownloadingInvoiceId(null); // Reset after the download completes
     }, 2000);
   };
@@ -210,18 +210,18 @@ const SubscriptionTable = ({ subscriptions, searchQuery }: any) => {
           </TableHeader>
           <TableBody
             items={paginatedData || []}
-            emptyContent={'No subscriptions found'}
+            emptyContent={"No subscriptions found"}
           >
             {(item) => (
               <TableRow key={item.id}>
                 <TableCell className="text-left font-medium">
                   {mapPlan(item.plan)}
                 </TableCell>
-                <TableCell>{renderCell(item, 'subscriptionEndDate')}</TableCell>
-                <TableCell>{mapPaymentPeriod(item.paymentPeriod)}</TableCell>
+                <TableCell>{renderCell(item, "subscriptionEndDate")}</TableCell>
                 <TableCell>{mapAmount(item.totalAmount)}</TableCell>
+                <TableCell>{mapPaymentPeriod(item.paymentPeriod)}</TableCell>
                 <TableCell className="text-center">
-                  {renderCell(item, 'invoice')}
+                  {renderCell(item, "invoice")}
                 </TableCell>
               </TableRow>
             )}
