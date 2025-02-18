977da5633d1c406acbdbbae0b01ff4e892cf19bf
Damilare Oyedeji <damoye81@gmail.com>
2025-02-18 16:38:32 +0100

    update

--- app/api/apiService.ts
+++ app/api/apiService.ts
@@ -1,146 +1,4 @@
-// import { refreshToken } from "./apiService";
-// import {
-//   getJsonItemFromLocalStorage,
-//   notify,
-//   removeCookie,
-//   resetLoginInfo,
-//   saveJsonItemToLocalStorage,
-// } from "@/lib/utils";
-// import axios, { AxiosError } from "axios";
-
-// import { generateRefreshToken, logout } from "./controllers/auth";
-
-// const api = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   timeout: 20000,
-// });
-
-// api.interceptors.request.use(async (config) => {
-//   const userData = getJsonItemFromLocalStorage("userInformation");
-//   const business = getJsonItemFromLocalStorage("business");
-//   const token = userData?.token;
-//   const cooperateID = userData?.cooperateID;
-//   const businessId = business?.businessId;
-
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-
-//   if (cooperateID) {
-//     config.headers["cooperateId"] = cooperateID;
-//   }
-
-//   if (businessId) {
-//     config.headers["businessId"] = businessId;
-//   }
-
-//   const isMultipartFormData =
-//     config.headers["Content-Type"] === "multipart/form-data";
-//   if (isMultipartFormData) {
-//     delete config.headers["Content-Type"];
-//   }
-
-//   return config;
-// });
-
-// api.interceptors.response.use(
-//   (response) => response,
-//   async (error) => {
-//     let originalConfig = error.config;
-
-//     if (
-//       error.response &&
-//       error.response.status === 401 &&
-//       !originalConfig._retry
-//     ) {
-//       originalConfig._retry = true;
-//       try {
-//         const userData = getJsonItemFromLocalStorage("userInformation");
-//         const businesses = getJsonItemFromLocalStorage("business");
-
-//         const { refreshToken, email } = userData;
-//         const businessId = businesses[0].businessId;
-
-//         const rs = await generateRefreshToken({
-//           refreshToken,
-//           businessId,
-//           email,
-//         });
-
-//         if (!rs) {
-//           resetLoginInfo();
-//           throw new Error("Failed to generate new token");
-//         }
-//         console.log(rs, "!rs");
-//         const {
-//           token: newToken,
-//           refreshToken: newRefreshToken,
-//           tokenExpiration: newExpiration,
-//         } = rs.data.data;
-
-//         saveJsonItemToLocalStorage("userInformation", {
-//           ...userData,
-//           token: newToken,
-//           refreshToken: newRefreshToken,
-//           tokenExpiration: newExpiration,
-//         });
-
-//         api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-//         return api(originalConfig);
-//       } catch (_error) {
-//         resetLoginInfo();
-//         if (error.response && error.response.data) {
-//           resetLoginInfo();
-//           return Promise.reject(error.response.data);
-//         }
-//         return Promise.reject(_error);
-//       }
-//     } else {
-//       handleError(error, false);
-//     }
-//     return Promise.reject(error);
-//   }
-// );
-
-// export const handleError = (error: any, showError: boolean = true) => {
-//   if (showError) {
-//     if (!error.response?.data?.title) {
-//       notify({
-//         title: "Error!",
-//         text:
-//           error.response?.data?.error?.responseDescription ||
-//           "An error occurred",
-//         type: "error",
-//       });
-//     } else if (error.code === "ECONNABORTED") {
-//       notify({
-//         title: "Network Timeout",
-//         text: "The request took too long. Please try again later.",
-//         type: "error",
-//       });
-//     } else if (error.code === "ERR_NETWORK") {
-//       notify({
-//         title: "Network Error!",
-//         text: "Check your network and try again",
-//         type: "error",
-//       });
-//     } else {
-//       notify({
-//         title: "Error!",
-//         text: "An error occurred, please try again",
-//         type: "error",
-//       });
-//     }
-//   }
-// };
-
-// export default api;
-
-"use client";
-
+import { refreshToken } from "./apiService";
 import {
   getJsonItemFromLocalStorage,
   notify,
@@ -149,63 +7,9 @@ import {
   saveJsonItemToLocalStorage,
 } from "@/lib/utils";
 import axios, { AxiosError } from "axios";
+
+import { generateRefreshToken, logout } from "./controllers/auth";
 import toast from "react-hot-toast";
-import { generateRefreshToken } from "./controllers/auth";
-
-let refreshTimer: NodeJS.Timeout | null = null;
-
-export const refreshToken = async () => {
-  const userData = getJsonItemFromLocalStorage("userInformation");
-  if (!userData) return null;
-
-  const { token, email } = userData;
-
-  try {
-    const response = await generateRefreshToken({
-      token,
-      email,
-    });
-
-    const { jwtToken: newToken, tokenExpiration: newExpiration } =
-      response?.data?.data;
-
-    saveJsonItemToLocalStorage("userInformation", {
-      ...userData,
-      token: newToken,
-      tokenExpiration: newExpiration,
-    });
-
-    scheduleTokenRefresh();
-    return newToken;
-  } catch (error) {
-    toast.error("Session Expired, please log in again.");
-    resetLoginInfo();
-    return null;
-  }
-};
-
-export const scheduleTokenRefresh = () => {
-  if (refreshTimer) {
-    clearTimeout(refreshTimer);
-  }
-
-  const userData = getJsonItemFromLocalStorage("userInformation");
-  if (!userData || !userData.tokenExpiration) return;
-
-  const expirationTime = new Date(userData.tokenExpiration).getTime();
-  const currentTime = Date.now();
-
-  const timeUntilRefresh = expirationTime - currentTime - 60 * 1000;
-
-  if (timeUntilRefresh <= 0) {
-    refreshToken();
-    return;
-  }
-
-  refreshTimer = setTimeout(() => {
-    refreshToken();
-  }, timeUntilRefresh);
-};
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
@@ -245,10 +49,59 @@ api.interceptors.request.use(async (config) => {
 
 api.interceptors.response.use(
   (response) => response,
-  async (error: AxiosError) => {
-    if (error.response?.status === 401) {
-      toast.error("Session Expired, please log in again.");
-      resetLoginInfo();
+  async (error) => {
+    let originalConfig = error.config;
+
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalConfig._retry
+    ) {
+      originalConfig._retry = true;
+      try {
+        const userData = getJsonItemFromLocalStorage("userInformation");
+        const businesses = getJsonItemFromLocalStorage("business");
+
+        const { refreshToken, email } = userData;
+        const businessId = businesses[0].businessId;
+
+        const rs = await generateRefreshToken({
+          refreshToken,
+          businessId,
+          email,
+        });
+
+        if (!rs) {
+          resetLoginInfo();
+
+          throw new Error("Failed to generate new token");
+        }
+        console.log(rs, "!rs");
+        const {
+          token: newToken,
+          refreshToken: newRefreshToken,
+          tokenExpiration: newExpiration,
+        } = rs.data.data;
+
+        saveJsonItemToLocalStorage("userInformation", {
+          ...userData,
+          token: newToken,
+          refreshToken: newRefreshToken,
+          tokenExpiration: newExpiration,
+        });
+
+        api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+        return api(originalConfig);
+      } catch (_error) {
+        resetLoginInfo();
+        toast.error("Session Expired, please log in again.");
+        if (error.response && error.response.data) {
+          resetLoginInfo();
+
+          return Promise.reject(error.response.data);
+        }
+        return Promise.reject(_error);
+      }
     } else {
       handleError(error, false);
     }
@@ -287,7 +140,5 @@ export const handleError = (error: any, showError: boolean = true) => {
     }
   }
 };
-
-scheduleTokenRefresh();
 
 export default api;
