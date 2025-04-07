131cbd063081ac099bc8cffc1e1f747383638237
Damilare Oyedeji <damoye81@gmail.com>
2025-04-07 20:51:53 +0100

    update

--- hooks/cachedEndpoints/useReservation.tsx
+++ hooks/cachedEndpoints/useReservation.tsx
@@ -6,29 +6,35 @@ import {
 import { getJsonItemFromLocalStorage } from '@/lib/utils';
 import { useQuery } from 'react-query';
 import { useGlobalContext } from '../globalProvider';
+import { fetchQueryConfig } from "@/lib/queryConfig";
 
 const useReservation = (businessIdOutsideApp?: any, cooperateID?: any) => {
   const { page, rowsPerPage } = useGlobalContext();
-  const businessInformation = getJsonItemFromLocalStorage('business');
+  const businessInformation = getJsonItemFromLocalStorage("business");
   const businessId = businessInformation
     ? businessInformation[0]?.businessId
     : businessIdOutsideApp;
   const getAllReservation = async ({ queryKey }) => {
-    const responseData = await getReservations(
-      businessId,
-      page,
-      rowsPerPage,
-      cooperateID
-    );
-    return responseData?.data?.data as payloadReservationItem[];
+    try {
+      const responseData = await getReservations(
+        businessId,
+        page,
+        rowsPerPage,
+        cooperateID
+      );
+      return (responseData?.data?.data as payloadReservationItem[]) ?? [];
+    } catch (error) {
+      return [];
+    }
   };
 
   const { data, isLoading, isError, refetch } = useQuery<
     payloadReservationItem[]
-  >(['reservation', { page, rowsPerPage }], getAllReservation, {
-    keepPreviousData: true,
-    refetchOnWindowFocus: false,
-  });
+  >(
+    ["reservation", { page, rowsPerPage }],
+    getAllReservation,
+    fetchQueryConfig()
+  );
 
   return {
     data,
