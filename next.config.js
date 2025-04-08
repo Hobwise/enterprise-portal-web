86c5db45fd9a0659ef97e5fb49c647e1cf140f9f
Damilare Oyedeji <damoye81@gmail.com>
2025-04-08 18:26:38 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  publicRuntimeConfig: {
+    apiBaseUrl:
+      process.env.NODE_ENV === "production"
+        ? "https://api.hobwise.com/"
+        : "https://sandbox-api.hobwise.com/",
+  },
   reactStrictMode: true,
   headers: async () => {
     return [
@@ -12,11 +18,11 @@ const nextConfig = {
               "default-src 'self'; " +
               "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
               "style-src 'self' 'unsafe-inline'; " +
-              "img-src 'self' data: https://walrus-app-lehim.ondigitalocean.app https://hobwise.com https://hobwise-corporate-web.vercel.app https://sandbox.hobwise.com; " +
-              "connect-src 'self' https://walrus-app-lehim.ondigitalocean.app https://hobwise.com https://hobwise-corporate-web.vercel.app https://sandbox.hobwise.com; " +
-              "font-src 'self' https://hobwise.com https://hobwise-corporate-web.vercel.app https://sandbox.hobwise.com; " +
-              "frame-src https://hobwise.com https://hobwise-corporate-web.vercel.app https://sandbox.hobwise.com; " +
-              "media-src https://hobwise.com https://hobwise-corporate-web.vercel.app https://sandbox.hobwise.com;",
+              "img-src 'self' data: https://walrus-app-lehim.ondigitalocean.app https://hobwise.com https://hobwise-corporate-web.vercel.app https://api.hobwise.com https://sandbox-api.hobwise.com https://sandbox.hobwise.com; " +
+              "connect-src 'self' https://walrus-app-lehim.ondigitalocean.app https://hobwise.com https://hobwise-corporate-web.vercel.app https://api.hobwise.com https://sandbox-api.hobwise.com https://sandbox.hobwise.com; " +
+              "font-src 'self' https://hobwise.com https://hobwise-corporate-web.vercel.app https://api.hobwise.com https://sandbox-api.hobwise.com https://sandbox.hobwise.com; " +
+              "frame-src https://hobwise.com https://hobwise-corporate-web.vercel.app https://api.hobwise.com https://sandbox-api.hobwise.com https://sandbox.hobwise.com; " +
+              "media-src https://hobwise.com https://hobwise-corporate-web.vercel.app https://api.hobwise.com https://sandbox-api.hobwise.com https://sandbox.hobwise.com;",
           },
           {
             key: "X-Frame-Options",
