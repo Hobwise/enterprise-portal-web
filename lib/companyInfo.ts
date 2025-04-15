820e19b28b541dfc156b779e9a9a843f39af573d
Damilare Oyedeji <damoye81@gmail.com>
2025-04-15 18:38:29 +0100

    update

--- lib/companyInfo.ts
+++ lib/companyInfo.ts
@@ -3,5 +3,5 @@ import Hobwise from "@/public/assets/images/hobwise.png";
 export const companyInfo = {
   name: "Hobwise",
   logo: Hobwise,
-  webUrl: "https://sandbox.hobwise.com",
+  webUrl: process.env.NEXT_PUBLIC_WEB_BASE_URL,
 };
