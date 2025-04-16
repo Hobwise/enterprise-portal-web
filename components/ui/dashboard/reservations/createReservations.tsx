3b63f1adf1bbc943c14afaaca3f370497a7634ee
Damilare Oyedeji <damoye81@gmail.com>
2025-04-16 12:26:01 +0100

    update

--- components/ui/dashboard/reservations/createReservations.tsx
+++ components/ui/dashboard/reservations/createReservations.tsx
@@ -14,24 +14,24 @@ const CreateReservation = () => {
   return (
     <section>
       <Spacer y={14} />
-      <div className='flex flex-col items-center'>
-        <Image src={NoBooking} alt='no menu illustration' />
+      <div className="flex flex-col items-center">
+        <Image src={NoBooking} alt="no menu illustration" />
         <Spacer y={5} />
-        <p className='text-lg font-[600]'>You have no Reservations yet</p>
-        <p className='text-sm font-[400] xl:w-[260px] w-full text-center text-[#475367]'>
-          Create Quick response so that customers can start booking
+        <p className="text-lg font-[600]">You have no Reservations yet</p>
+        <p className="text-sm font-[400] xl:w-[260px] w-full text-center text-[#475367]">
+          Create reservations so that customers can start booking
         </p>
         <Spacer y={5} />
         {(role === 0 || userRolePermissions?.canCreateReservation === true) && (
           <CustomButton
             onClick={() =>
-              router.push('/dashboard/reservation/create-reservation')
+              router.push("/dashboard/reservation/create-reservation")
             }
-            className='py-2 px-4 md:mb-0 mb-4 text-white'
-            backgroundColor='bg-primaryColor'
+            className="py-2 px-4 md:mb-0 mb-4 text-white"
+            backgroundColor="bg-primaryColor"
           >
-            <div className='flex gap-2 items-center justify-center'>
-              <IoMdAdd className='text-[22px]' />
+            <div className="flex gap-2 items-center justify-center">
+              <IoMdAdd className="text-[22px]" />
 
               <p>Create reservation</p>
             </div>
